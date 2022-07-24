dd320cb102a42be81d6934cb848f957210036a1f
Alexander Makarov <fsbisan@mail.ru>
2022-07-24 13:48:51 +0500

    Created StartTableView

---
+++ BreakingBadApp/BreakingBadApp/Workers/NetworkManager.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkManager.swift
+//  BreakingBadApp
+//
+//  Created by Александр Макаров on 24.07.2022.
+//
+
+import Foundation
+
+class NetworkManager {
+    static var shared = NetworkManager()
+    
+    
+    
+    private init() {}
+}
